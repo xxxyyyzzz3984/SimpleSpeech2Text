1d2705cf5dbe200c70a302b11a9b6bdeb6091c6e
yinhaoxiao <xyh3984@gwu.edu>
2020-05-10 08:41:41 -0400

    :green_heart:Infinitely running speech2text

--- Speech2TextMain.py
+++ Speech2TextMain.py
@@ -3,22 +3,33 @@ from os import listdir
 from os.path import isfile, join
 import re
 from bcolors import bcolors
+import time
 
 SoundWavDir = "./wav/"
 TextDir = "./text/"
 BeginCount = 0
 soundfiles = [f for f in listdir(SoundWavDir) if isfile(join(SoundWavDir, f))]
+textfiles = [f for f in listdir(TextDir) if isfile(join(TextDir, f))]
 
 if __name__ == '__main__':
+    while True:
+        for soundfile in soundfiles:
+            file_index = re.findall(r"\d+", soundfile)[0]
+            if int(file_index) < BeginCount:
+                continue
+            textfilename = "%d.txt" % int(file_index)
+            if textfilename in textfiles:
+                print(bcolors.WARNING + "record%d.wav already processed, skip" % int(file_index) + bcolors.ENDC)
+                continue
+            soundfile_path = SoundWavDir + "record%d.wav" % int(file_index)
+            text_result = speech2text_wav(soundfile_path)
+            print(bcolors.HEADER + "Writing Text to %d.txt." % int(file_index))
+            with open(TextDir + "%d.txt" % int(file_index), "w") as f:
+                f.write(text_result)
+                f.close()
 
-    for soundfile in soundfiles:
-        file_index = re.findall(r"\d+", soundfile)[0]
-        if int(file_index) < BeginCount:
-            continue
-        soundfile_path = SoundWavDir + "record%d.wav" % int(file_index)
-        text_result = speech2text_wav(soundfile_path)
+        print("Round finished. Sleep 30 seconds!")
+        time.sleep(30)
+        soundfiles = [f for f in listdir(SoundWavDir) if isfile(join(SoundWavDir, f))]
+        textfiles = [f for f in listdir(TextDir) if isfile(join(TextDir, f))]
 
-        print(bcolors.HEADER + "Writing Text to %d.txt." % int(file_index))
-        with open(TextDir + "%d.txt" % int(file_index), "w") as f:
-            f.write(text_result)
-            f.close()
